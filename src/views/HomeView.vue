0231c364735da1bad930e45f492a4ac6fb87e895
petrero <petrero@gmail.com>
2024-01-09 11:57:15 +0200

    18.8 You no longer have to open the component and register it. You can get get rid of the components object instantly. All I have to do is to import the component and it's magically available in the template.  I get rid of the export default object entirely. I don't need the return from setup method either. All i have to do is define the message and it's magically available in our template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,12 @@
 <script setup>
   import TheWelcome from '../components/TheWelcome.vue';
-  import { onMounted, ref } from "vue";
+  import { ref } from "vue";
 
-  export default {
-    components: { TheWelcome },
-    setup() {
-      let message = ref('Hello World!');
+  let message = ref('Hello World!');
 
-      setTimeout(() => {
-        message.value = 'I have been changed'
-      }, 2000);
-
-      return {
-        message
-      }
-    }
-    /*data() {
-      return {
-        message: 'Hello World'
-      }
-    },
-    mounted() {
-      alert('mounted');
-    }*/
-  }
+  setTimeout(() => {
+    message.value = 'I have been changed'
+  }, 2000);
 </script>
 
 <template>
