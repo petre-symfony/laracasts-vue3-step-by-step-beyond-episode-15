347130481a4a65b94bf5504dd564c2060c9ac98a
petrero <petrero@gmail.com>
2024-01-09 11:04:44 +0200

    18.1 Reproduce script setup markup in a way that we are comfortable with. Everithing works like before.  In vue there are two different ways that we can organize and structure our code. Originally we only had the options api. It's not deprecated, it's not going away, it's just slightly different way of structuring your code base with some pros and some cons associated with it.
     With Composition API, we define a component's logic using imported API functions. In SFCs, Composition API is typically used with <script setup>.
    
     Setup is just an attribute that we add to the script tag that behind the scenes the compiler will pick up on and then apply a series of transformations to make everithing work the way we expect. The browser is making use of a hook basically, a macro that will compile everything down to provide a bit more convenience.
    
     Composition API is a set of APIs that allows us to author Vue components using imported functions instead of declaring options. It is an umbrella term that covers the following APIs
     - Reactivity API, e.g. ref() and reactive(), that allows us to directly create reactive state, computed state, and watchers.
     - Lifecycle Hooks, e.g. onMounted() and onUnmounted(), that allow us to programmatically hook into the component lifecycle.
     - Dependency Injection, i.e. provide() and inject(), that allow us to leverage Vue's dependency injection system while using Reactivity APIs.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+  import TheWelcome from '../components/TheWelcome.vue';
+
+  export default {
+    components: { TheWelcome },
+    data() {
+      return {
+        message: 'Hello World'
+      }
+    },
+    mounted() {
+      alert('mounted');
+    }
+  }
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
   </main>
 </template>
