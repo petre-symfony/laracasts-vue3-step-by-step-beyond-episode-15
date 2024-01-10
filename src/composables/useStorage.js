ff7c62a10fc13ffeeef30c9d36f3e14a9365e29f
petrero <petrero@gmail.com>
2024-01-10 13:12:53 +0200

    20.9 Okay, the next step is to take with a user types and also write it to local storage.
     So let's switch back and we learned that we could add a watcher.
    
     So I will import watch from view remember, this is how the composition API works we import the apis that we require.
    
     So let's watch that value and when it changes.
    
     I want to write to local storage.
    
     Okay, let's switch back and find our right function that we wrote earlier. I'll cut that out and bring it. How about right down here?
    
     But this time write doesn't need the key and value. We already have it. So let's get rid of that. The key was passed in when we call the function and the value is our ref here. So just keep in mind when you have a ref you always got attack on dot value which makes this property name a little weird vowel dot value. Maybe we can think of a better name later. But nonetheless, I think this should work. All right, so think about it. Let's go through this at the very top I say, all right, I want to use storage for food so immediately it looks in local storage to see if we have anything for it. And if we do we wrap it in a ref.
    
     And in fact, if we don't we also wrap it in a ref, but it's just set to null then we keep an eye on that model and if it changes we instantly call this right function that writes the value back to local storage.
    
     All right. So let's give this a shot now. I'll go into application. It looks like my local storage is clear. I will type pizza into it and now we're up and running. So have a look. We are keeping food and sink.
    
     Which means if I were to change it here to Tacos. Well that change should also trigger a right to local storage. So back to application and now notice that's been updated. All right, pretty cool.

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -1,9 +1,17 @@
-import {ref} from "vue";
+import { ref, watch } from "vue";
 
 export function useStorage(key) {
   let storeVal = localStorage.getItem(key);
 
   let val = ref(storeVal);
-  
+
+  watch(val, () => {
+    write()
+  })
+
+  function write() {
+    localStorage.setItem(key, val.value);
+  }
+
   return val
 }
