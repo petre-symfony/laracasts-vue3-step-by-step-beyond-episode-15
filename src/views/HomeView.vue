85db16b17f9ea9896f48cafab30048f74fafafe9
petrero <petrero@gmail.com>
2024-01-10 12:37:25 +0200

    20.7 So you can see something as simple as remembering basic input data. Yeah, it gets kind of annoying to write. So instead. I think this is a perfect use case for extracting a reusable composable.
     Okay. So open up the sidebar. I will add a new composable. I'm going to call it use storage.
    
     And as we've learned I'll hide the sidebar. We should export a function with that same name.
    
     That will return an object that we can receive from our component. All right. So let's switch back to home view. We now know that we're going to import use storage from our composable.
    
     And then ideally if I get rid of all of this, yeah, I want to say something like all right, I want to use storage for food.
    
     And then I will catch that into this variable. Yeah, ideally, that's all I need to do here. And as you can imagine this will look into local storage. It will try to find something for the Key Food if it does it returns it and maybe it should also return it as a ref.
    
     That way we can instantly use it within our input.
    
     Okay, so let's keep it very simple for now get rid of all of that. And if we try it of course nothing is going to happen because you use storage is blank. So let's write a little code before trying this out.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
  import { ref, watch } from "vue";
+ import { useStorage } from "@/composables/useStorage.js";
 
- let food = ref(localStorage.getItem('food'));
+ let food = useStorage('food')
+ /*let food = ref(localStorage.getItem('food'));
  let age = ref(localStorage.getItem('age'));
 
  function write(key, value) {
@@ -19,16 +21,14 @@
  setTimeout(() => {
    food.value = 'changed'
  }, 2000)
+
+  */
 </script>
 
 <template>
   <main>
     <p>
       What is your favorite food? <input type="text" v-model="food" >
-    </p>
-
-    <p>
-      How old are you? <input type="text" v-model="age" >
     </p>
   </main>
 </template>
