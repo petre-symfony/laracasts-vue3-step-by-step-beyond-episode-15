daf12f68497980eb362418e8d065549d5bb83353
petrero <petrero@gmail.com>
2024-01-10 20:45:47 +0200

    21.10 The next step is to extract a dedicated component.  We don't want a default value and we don't want a hard code width and height you should pass them from outside
     Switch back to the homeview. We can now import TabbableTextarea component. It works but now we abstracted all the logic for tabbing

---
+++ src/components/TabbableTextarea.vue
@@ -0,0 +1,17 @@
+<script setup>
+function onTabPress(e) {
+  let textarea = e.target;
+
+  let val = textarea.value,
+      start = textarea.selectionStart,
+      end = textarea.selectionEnd;
+
+  textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+  textarea.selectionStart = textarea.selectionEnd = start + 1;
+}
+</script>
+
+<template>
+  <textarea @keydown.tab.prevent="onTabPress" ></textarea>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,11 @@
 <script setup>
-
-  function onTabPress(e) {
-    let textarea = e.target;
-
-    // get caret position/selection
-    let val = textarea.value,
-        start = textarea.selectionStart,
-        end = textarea.selectionEnd;
-
-    //set textarea value to: text before caret + tab + text after caret
-    textarea.value = val.substring(0, start) + "\t" + val.substring(end);
-
-    //got caret at right position again
-    textarea.selectionStart = textarea.selectionEnd = start + 1;
-  }
+  import TabbableTextarea from "@/components/TabbableTextarea.vue";
 </script>
 
 <template>
   <main>
     <form >
-      <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px">Hi there</textarea>
+      <TabbableTextarea style="width: 100%; height: 300px"/>
     </form>
   </main>
 </template>
