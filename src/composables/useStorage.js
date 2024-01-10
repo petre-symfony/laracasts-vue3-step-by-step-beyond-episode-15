afabd3a0a3d4e62defe9abc1d7df66ecbb6f3cad
petrero <petrero@gmail.com>
2024-01-10 12:44:41 +0200

    20.8 We're going to accept a key and we've learned that we can run local storage dot get item for key. Now if there is anything in local storage for that key it of course returns to the value. Otherwise, they'll return to null.
     So why don't we save this to a variable like stored value whatever is in local storage.
    
     But then ultimately I want to return a ref to the user.
    
     So I could say let value equal ref import that stored value and then ultimately return this value to the user.
    
     All right. So if we switch back, we now know that this food variable is at the very least reactive which means if we had V model to it we can keep track of what the user types in.
    
     So open up view Dev tools. Here's our home view right now food is null we type into it. And yeah, we are tracking that which is great.

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -1,3 +1,9 @@
-export function useStorage() {
-  return {}
+import {ref} from "vue";
+
+export function useStorage(key) {
+  let storeVal = localStorage.getItem(key);
+
+  let val = ref(storeVal);
+  
+  return val
 }
