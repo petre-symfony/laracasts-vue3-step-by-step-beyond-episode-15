c4180f37424575fb966e626a283f29a4a561abb7
petrero <petrero@gmail.com>
2024-01-10 20:29:07 +0200

    21.9 Another option if you want is you don't need ref. You can listen for the keydown event and call onTabPress and then you can grab the text area by saying e.target. If i'm pressing the tab within the textarea the target of the event is the textarea node. So I could just as easily access it like so.  Now we just have a simple function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,25 @@
 <script setup>
-  import {ref, onMounted } from "vue";
-
-  let textarea = ref(null);
 
   function onTabPress(e) {
-    let t = textarea.value;
+    let textarea = e.target;
 
     // get caret position/selection
-    let val = t.value,
-        start = t.selectionStart,
-        end = t.selectionEnd;
+    let val = textarea.value,
+        start = textarea.selectionStart,
+        end = textarea.selectionEnd;
 
     //set textarea value to: text before caret + tab + text after caret
-    t.value = val.substring(0, start) + "\t" + val.substring(end);
+    textarea.value = val.substring(0, start) + "\t" + val.substring(end);
 
     //got caret at right position again
-    t.selectionStart = t.selectionEnd = start + 1;
+    textarea.selectionStart = textarea.selectionEnd = start + 1;
   }
 </script>
 
 <template>
   <main>
     <form >
-      <textarea ref="textarea" @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px">Hi there</textarea>
+      <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px">Hi there</textarea>
     </form>
   </main>
 </template>
