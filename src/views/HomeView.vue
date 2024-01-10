fd502c510dc56d6d4cd7acca20c109ca0422cb6d
petrero <petrero@gmail.com>
2024-01-10 20:18:07 +0200

    21.7 We can tell onkeydown event to listen for tab adding tab modifier. Now will only call the function only if the user presses the tab which means if clause is unnecesary.  In fact onKeyDown is no longer quite right. Let's say it onTabPress.  Eveything is working like before.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,30 +3,29 @@
 
   let textarea = ref(null);
 
-  function onKeyDown(e) {
+  function onTabPress(e) {
     let t = textarea.value;
-    //tab was pressed
-    if (e.keyCode === 9) {
-      // get caret position/selection
-      let val = t.value,
-          start = t.selectionStart,
-          end = t.selectionEnd;
 
-      //set textarea value to: text before caret + tab + text after caret
-      t.value = val.substring(0, start) + "\t" + val.substring(end);
+    // get caret position/selection
+    let val = t.value,
+        start = t.selectionStart,
+        end = t.selectionEnd;
 
-      //got caret at right position again
-      t.selectionStart = t.selectionEnd = start + 1;
+    //set textarea value to: text before caret + tab + text after caret
+    t.value = val.substring(0, start) + "\t" + val.substring(end);
 
-      e.preventDefault();
-    }
+    //got caret at right position again
+    t.selectionStart = t.selectionEnd = start + 1;
+
+    e.preventDefault();
+
   }
 </script>
 
 <template>
   <main>
     <form >
-      <textarea ref="textarea" @keydown="onKeyDown" style="width: 100%; height: 300px">Hi there</textarea>
+      <textarea ref="textarea" @keydown.tab="onTabPress" style="width: 100%; height: 300px">Hi there</textarea>
     </form>
   </main>
 </template>
