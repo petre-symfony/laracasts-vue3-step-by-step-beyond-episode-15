df997343c47cad7a1d1495dd24cd6c23b97967cc
petrero <petrero@gmail.com>
2024-01-10 19:27:28 +0200

    21.3 integrate the code into component. The first step you need some kind of textarea.  Let's start by getting a ref. Let's call it textarea and then create it in script setup.  If you ever want to acces a specific element in DOM node in the template just give it a ref, provide any name you want and then create a variable equal to a null ref.  If i console.log(textarea.value) that's going to be null because at the point we logged that the textarea has not been yet mounted to the page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup>
-  textarea.addEventListener('keydown', (e) => {
+  import {ref} from "vue";
+
+  let textarea = ref(null);
+  console.log(textarea.value) //null, the textarea has not been yet mounted to the page
+  /*textarea.addEventListener('keydown', (e) => {
    //tab was pressed
    if (e.keyCode === 9) {
      // get caret position/selection
@@ -15,13 +19,13 @@
 
      e.preventDefault();
    }
-  });
+  });*/
 </script>
 
 <template>
   <main>
     <form >
-      <textarea style="width: 100%; height: 300px">Hi there</textarea>
+      <textarea ref="textarea" style="width: 100%; height: 300px">Hi there</textarea>
     </form>
   </main>
 </template>
