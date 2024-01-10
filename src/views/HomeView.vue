d34a9fa1a9a23b6c2b928e101bdfee91791eadd9
petrero <petrero@gmail.com>
2024-01-10 12:18:06 +0200

    20.4 But now let's say I want to do this for multiple inputs so I could say I don't know. How old are you and this will be V model for H.
     Alright, let's set that up. See how it might work. We now have one for age where we get a key called age from local storage.
    
     Next the right function can't be so generic. We're going to need a key as well as a value to write to that way. I can substitute these.
    
     Okay. So now when we type into this input, we could write two food the value of food. All right, and then the same thing here for age.
    
     All right, let's give that one a shot. So we'll say here pizza. Maybe you're 12 years old. We give it a refresh and that has been saved. And once again if we go into local storage sure enough. We are tracking that let's do my age 37 Tacos, you get the idea. Everything is working here.
    
     Okay, but it is a little bit messy, but then there's so much more we need to deal with things like serialization.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,21 @@
  import { ref } from "vue";
 
  let food = ref(localStorage.getItem('food'));
+ let age = ref(localStorage.getItem('age'));
 
- function write() {
-   localStorage.setItem('food', food.value);
+ function write(key, value) {
+   localStorage.setItem(key, value);
  }
 </script>
 
 <template>
   <main>
     <p>
-      What is your favorite food? <input type="text" v-model="food" @input="write">
+      What is your favorite food? <input type="text" v-model="food" @input="write('food', food)">
+    </p>
+
+    <p>
+      How old are you? <input type="text" v-model="age" @input="write('age', age)">
     </p>
   </main>
 </template>
