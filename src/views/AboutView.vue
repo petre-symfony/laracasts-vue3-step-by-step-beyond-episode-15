e01335be7b83bfd1798057b1b028cee1e9e16c47
petrero <petrero@gmail.com>
2024-01-12 10:33:50 +0200

    24.5 Our tangible I only bring up these terms because once we in the next episode review a tool called Pina you're going to see these terms like Getters actions States, but they're really just data methods computer properties kind of the same thing. So yeah, think about it. Now. If we need to access the current count from any of the pages in our application, it's really easy to do. So, whereas if we had instead stored it all on counterview, like maybe you had something like this. Well, then it's not so easy because as soon as I switch to a different page, the counterview is swapped out with a totally different View and we no longer have access to that to that data. So let's just have a look. Maybe the user goes to about page. We're going to import our counter.
     And now I can say the current count is counter dot count.
    
     All right. Let's see. Give it a refresh. Let's go to number six and if I switch to about yes, we have access to that state and that is the huge benefit to an approach like this. Okay, so hopefully you're getting a little bit more comfortable with creating your own stores. And again, I want to stress. You know, what this might be all you need.
    
     It just depends on what you're building but there are some benefits to using a dedicated tool. So with that in mind in the next episode, I promise I'm going to show you a tool called Pina.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { useFlash } from "@/composables/useFlash.js";
+  import { counter } from "@/stores/conterStore.js";
 
   let { flash } = useFlash();
 </script>
@@ -8,6 +9,7 @@
   <div>
     <p>
       <button @click="flash('Hey!', 'It works on the about page!', 'info')">Click Me</button>
+      The current count is {{ counter.count }}
     </p>
   </div>
 </template>
